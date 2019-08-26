e832821c91ff717800a77763addc2ebab9d84ecd
Samuel Waugh <swaugh@rodeofx.com>
2019-08-26 11:16:48 -0400

    added doc

--- WordSensitiveChunker/testcunk.py
+++ WordSensitiveChunker/testcunk.py
@@ -1,6 +1,17 @@
+"""
+Example to chunk a sentence into 10 characters, rounding up to the nearest word.
+"""
+#pylint: disable=missing-yield-doc,missing-yield-type-doc,invalid-name
+def chunksToMaxChars(s, chunksize):
+    """
+    Chunk string to number of characters, rounding up to nearest space.
 
-def chunksToMaxChars(s):
-    if len(s) < 10:
+    :param string s: string to chunk
+    :param int chunksize: number of chars to chunk to
+
+    :yields str: chunk of given string
+    """
+    if len(s) < chunksize:
         yield s
         return
     splitstr = s.split(" ")
@@ -8,11 +19,12 @@ def chunksToMaxChars(s):
     i = 0
     while i < len(splitstr):
         beginOffset = i
-        while len(" ".join(splitstr[beginOffset:i])) < 10 and i < len(splitstr):
+        while len(" ".join(splitstr[beginOffset:i])) < chunksize and i < len(splitstr):
             i += 1
         yield " ".join(splitstr[beginOffset:i])
 
 
-
-for chunk in chunksToMaxChars("Hello there my good friend. How are you on this fine day?"):
+# Test iterator
+CHUNKSIZE = 10
+for chunk in chunksToMaxChars("Hello there my good friend. How are you on this fine day?", CHUNKSIZE):
     print("CHUNKED: %s" % chunk)
