50c0125e962c5e75b7d3db798ca6080b8b7a914c
Samuel Waugh <swaugh@rodeofx.com>
2019-09-09 10:34:54 -0400

    subprocessdoc

--- MultiProcessShare/multi2test.py
+++ MultiProcessShare/multi2test.py
@@ -4,6 +4,9 @@ Connect to a remote process and fetch a shared complex object.
 from multiprocessing.managers import BaseManager
 
 def test_connect_remote():
+    """
+    Connect to remote process and load complex object.
+    """
     BaseManager.register('get_my_class')
     rpm = BaseManager(address=('', 50000), authkey=b'abracadabra')
     rpm.connect()
