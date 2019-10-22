f4c659950d191ac5451758917ef09b3c31cc6d5b
Samuel Waugh <swaugh@rodeofx.com>
2019-10-22 11:28:27 -0400

    linted up translatos

--- EvilTranslate/translatetos.py
+++ EvilTranslate/translatetos.py
@@ -6,7 +6,7 @@ import html
 import urllib.parse
 import requests
 
-agent = {
+AGENT = {
     'User-Agent':
         "Mozilla/4.0 (\
         compatible;\
@@ -19,19 +19,26 @@ agent = {
         )"
 }
 
-def wreq(to_translate, to_language="auto", from_language="auto"):
+def wreq(to_translate, to_language="eng", from_language="auto"):
     """
     Translate the text "to translate" into the specified langage from the specified language.
+
+    :param str to_translate: text to be translated
+    :param str to_language: language to translate to, defaults to english.
+    :param str from_language: language to translate from. Defaults to 'auto'. Results may vary.
+
+    :returns: translated text
+    :rtype: str
     """
     base_link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s"
     to_translate = urllib.parse.quote(to_translate)
     link = base_link % (to_language, from_language, to_translate)
     print(link)
-    response = requests.get(link, headers=agent)
+    response = requests.get(link, headers=AGENT)
     data = response.text
     expr = r'class="t0">(.*?)<'
     re_result = re.findall(expr, data)
-    if len(re_result) == 0:
+    if len(re_result) == 0:  #pylint: disable=len-as-condition
         result = ""
     else:
         result = html.unescape(re_result[0])
